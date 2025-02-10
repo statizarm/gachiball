fbdaeb4d94f39488fd5440c332df167be2825edc
Artemiy Shushuev <statiz.arm@gmail.com>
2025-02-11 00:41:33 +0300

    improvement: remove dead code

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -7,13 +7,9 @@
 
 int main() {
     NGameEngine::TGameEngine engine;
-    engine.init();
-
     NGachiBall::TGame game{&engine};
-    std::unique_ptr<NCamera::ICamera> camera = NCamera::CreateRotatingCamera(
-        {0.f, 0.f, 0.f}, glm::radians(30.f), 20.f
-    );
-    engine.bindCamera(camera.get());
+
+    engine.init();
     engine.run(&game);
     engine.deinit();
 
